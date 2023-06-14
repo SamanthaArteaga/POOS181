49320b65a43942498ef75726bfc56c3fd996a47e
SamanthaArteaga <121037868@upq.edu.mx>
2023-06-14 17:08:59 -0600

    Conexión
    Se hizo la conexión a la base de datos

--- Practica5/app.py
+++ Practica5/app.py
@@ -1,8 +1,18 @@
 #importación del framework
 from flask import Flask
+from flask_mysqldb import MySQL
+
 
 #Inicialización del APP
 app= Flask(__name__)
+
+#Configuración a base de datos
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='dbflask'
+mysql= MySQL(app)
+
 
 #Declaración de ruta http://localhost:5000
 @app.route('/')
@@ -23,3 +33,4 @@ def eliminar():
 #Ejecución del Servidor en el puerto 5000
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+
